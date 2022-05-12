e6a1db53fde6852094a1b664839fe8b7da5983cc
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-05-12 19:18:28 +0430

    getting audiobooks informations from the html

--- fidiboscraper/fidiboscraper/spiders/fidibo_scraper.py
+++ fidiboscraper/fidiboscraper/spiders/fidibo_scraper.py
@@ -59,6 +59,135 @@ class FidiboScraperSpider(scrapy.Spider):
         if book_type == "مطالعه نسخه نمونه":
             pass
         else:
-            pass
-        
+            output_dict['type'] = "کتاب صوتی"
+
+
+            author = response.xpath(
+                "/html/body/main/div[2]/article/div[1]/div/div[2]/div/div/div[1]/ul/li[1]/a/span/text()"
+            ).getall()
+            if author is None or len(author)==0:
+                author = "اطلاعات موجود نیست."
+            else:
+                author = ','.join(author)            
+            output_dict["author"] = author
+
+
+            translator = response.xpath(
+                "/html/body/main/div[2]/article/div[1]/div/div[2]/div/div/div[1]/ul[1]/li[2]/a/span/text()"
+            ).getall()
+            if translator is None or len(translator)==0:
+                translator = "اطلاعات موجود نیست."
+            else:
+                translator = ','.join(translator)            
+            output_dict["translator"] = translator
+
+
+            narrator = response.xpath(
+                "/html/body/main/div[2]/article/div[1]/div/div[2]/div/div/div[1]/ul[1]/li[3]/a/span/text()"
+            ).getall()
+            if narrator is None or len(narrator)==0:
+                narrator = "اطلاعات موجود نیست."
+            else:
+                narrator = ','.join(narrator)            
+            output_dict["narrator"] = narrator
+
+
+            price = response.xpath(
+                "/html/body/main/div[2]/article/div[1]/div/div[3]/div/div/span/text()"
+            ).get()
+            if price is None:
+                price = "اطلاعات موجود نیست."
+            output_dict["price"] = price
+
+
+            publisher_img = response.xpath(
+                "/html/body/main/div[2]/section/div/div/ul/li[1]/img/@alt"
+            ).get()
+            if publisher_img == "ناشر":
+                publisher = response.xpath(
+                    "/html/body/main/div[2]/section/div/div/ul/li[1]/a/text()"
+                ).get()
+                if publisher is None:
+                    publisher = "اطلاعات موجود نیست."    
+                output_dict["publisher"] = publisher
+            else:
+                output_dict["publisher"] = "اطلاعات موجود نیست."
+
+            
+            output_dict["pvp"] = "اطلاعات موجود نیست."
+
+
+            publish_date_img = response.xpath(
+                "/html/body/main/div[2]/section/div/div/ul/li[2]/img/@alt"
+            ).get()
+            if publish_date_img == 'تاریخ نشر':
+                publish_date = response.xpath(
+                    "/html/body/main/div[2]/section/div/div/ul/li[2]/span/text()"
+                ).get()
+                if publish_date is None:
+                    publish_date = "اطلاعات موجود نیست."
+                output_dict["publish date"] = publish_date
+            else:
+                output_dict["publish date"] = "اطلاعات موجود نیست."
+
+            
+            language_img = response.xpath(
+                "/html/body/main/div[2]/section/div/div/ul/li[3]/img/@alt"
+            ).get()
+            if language_img == 'زبان':
+                language = response.xpath(
+                    "/html/body/main/div[2]/section/div/div/ul/li[3]/text()"
+                ).get()
+                if language is None:
+                    language = "اطلاعات موجود نیست."
+                output_dict["language"] = language
+            else:
+                output_dict["language"] = "اطلاعات موجود نیست."
+            
+
+            size_img = response.xpath(
+                "/html/body/main/div[2]/section/div/div/ul/li[4]/img/@alt"
+            ).get()
+            if size_img == "حجم فایل":
+                size = response.xpath(
+                    "/html/body/main/div[2]/section/div/div/ul/li[4]/text()"
+                ).get()
+                if size is None:
+                    size = "اطلاعات موجود نیست."
+                output_dict["size"] = size
+            else:
+                output_dict["size"] = "اطلاعات موجود نیست."
+
+            
+            output_dict["page count"] = "اطلاعات موجود نیست."
+            output_dict["ISBN"] = "اطلاعات موجود نیست."
+
+
+            description = response.xpath(
+                "/html/body/main/div[2]/article/section/div/div/div[1]/p/text()"
+            ).get()
+            if description is None:
+                description = "اطلاعات موجود نیست."
+            output_dict["description"] = description
+
+
+            book_cat = response.xpath(
+                "/html/body/div[1]/nav/ul/li/a/span/text()"
+            ).getall()
+            if book_cat is None:
+                book_cat = "اطلاعات موجود نیست."
+            else:
+                book_cat.pop(0)
+                book_cat.pop(len(book_cat)-1)
+                book_cat = ','.join(book_cat)
+            output_dict["category"] = book_cat
+
+
+            cover = response.xpath(
+                "/html/body/main/div[2]/article/div[1]/div/div[1]/div/img[1]/@src"
+            ).get()
+            if cover is None:
+                cover = "اطلاعات موجود نیست."
+            output_dict["cover"] = cover
+
         yield output_dict
